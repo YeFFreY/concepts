2a3f9d96a5ad9570d24e5ee4957bdefd2eedd048
YeFFreY <johan.mslms@outlook.com>
2019-09-05 14:59:09 +0200

    sample of dynamic formArray + comment on how the edit could work

--- src/app/library/activities/create/activity-create.component.ts
+++ src/app/library/activities/create/activity-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import {FormGroup, Validators, FormBuilder, FormArray} from '@angular/forms';
 import {ActivityService} from './activity.service';
 
 @Component({
@@ -16,8 +16,18 @@ import {ActivityService} from './activity.service';
                   <label>Overview</label>
                   <input type="text" formControlName="overview">
               </div>
+              <div formArrayName="activitySkills"
+              *ngFor="let activitySkill of activitySkills.controls; let i=index;">
+                <div [formGroupName]="i">
+                    <label attr.for="{{'skill' + i}}">Skill</label>
+                    <input id="{{'skill' + i}}" type="text" formControlName="skill">
+                    <label>Level</label>
+                    <input type="text" formControlName="level">
+                </div>
+              </div>
               <button type="submit" [disabled]="!activityForm.valid">Save</button>
               <button (click)="populate($event)">Populate</button>
+              <button (click)="addSkill($event)">Add Skill</button>
           </form>
           <br>Dirty : {{activityForm.dirty}}
           <br>Touched : {{activityForm.touched}}
@@ -29,6 +39,9 @@ import {ActivityService} from './activity.service';
 })
 export class ActivityCreateComponent implements OnInit {
   activityForm: FormGroup;
+  get activitySkills(): FormArray {
+    return this.activityForm.get('activitySkills') as FormArray;
+  }
 
   constructor(private fb: FormBuilder, private activityService: ActivityService) {
   }
@@ -36,7 +49,24 @@ export class ActivityCreateComponent implements OnInit {
   ngOnInit() {
     this.activityForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
-      overview: ''
+      overview: '',
+      activitySkills: this.fb.array([this.buildActivitySkill()])
+    });
+
+    // on edit, when the data is retrieved, populate the skills would be like (on a specific callback of the httpGet:
+/*
+    this.activityForm.patchValue({
+      title: this.activity.title;
+      overview: this.activity.overview
+    });
+    this.activityForm.setControl('activitySkills', this.fb.array(activity.skills || []));
+*/
+  }
+
+  buildActivitySkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      level: ''
     });
   }
 
@@ -53,5 +83,10 @@ export class ActivityCreateComponent implements OnInit {
     this.activityForm.patchValue({
       title: 'Populated title',
     });
+  }
+
+  addSkill(e) {
+    e.preventDefault();
+    this.activitySkills.push(this.buildActivitySkill());
   }
 }
